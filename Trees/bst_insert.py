bcdb591ebc4841d3eb00080bb4e67d6a96061420
Alfacodererzho <erzhigitcodingpy@gmail.com>
2025-01-26 22:29:33 -0600

    adding contains method

--- Trees/bst_insert.py
+++ Trees/bst_insert.py
@@ -30,3 +30,13 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
                 
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
